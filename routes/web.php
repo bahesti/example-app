2264d4dc246b83851d3695fc7b995a2873ef883a
muhamadbahesti <magusbahesti74@gmail.com>
2024-02-14 22:37:17 +0700

    Add features and pages

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('login', function () {
+    return view('pages.auth.login');
+});
+
+Route::get('sign-up', function () {
+    return view('pages.auth.sign-up');
+});
+
+Route::get('discussions', function () {
+    return view('pages.discussions.index');
+});
+
+Route::get('discussions/lorem', function () {
+    return view('pages.discussions.show');
+});
